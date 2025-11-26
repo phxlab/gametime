5d36083df80a3fd6dc7711ccf30e9f7477032a0d
Nico Plyley <nico@phoenixtechlab.com>
2025-11-25 20:26:37 -0500

    feat: add auth layout

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div
+	class="flex min-h-screen w-full items-center justify-center bg-gradient-to-br from-gray-50 to-white p-4 dark:from-gray-900 dark:to-gray-800"
+>
+	{@render children()}
+</div>
