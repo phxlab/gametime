93941c30bf3e2c4b6b76a5c2ebc581d422be0455
Nico Plyley <nico@phoenixtechlab.com>
2025-12-24 12:54:26 -0500

    refactor(admin): remove unused assignment to `locals.orgs` in load function

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -2,7 +2,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const orgs = await locals.Org.list();
-	locals.orgs = orgs;
 
 	return { orgs };
 };
