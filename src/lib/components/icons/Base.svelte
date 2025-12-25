0b57d608f485eb776e470ccb47b73412320c7368
Nico Plyley <nico@phoenixtechlab.com>
2025-12-25 17:15:26 -0500

    feat(icons): add new icon components

---
+++ src/lib/components/icons/Base.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const { children, ...restProps } = $props();
+</script>
+
+<svg
+	aria-hidden="true"
+	class="inline-block align-[-0.125em]"
+	viewBox="0 0 640 640"
+	width="1em"
+	height="1em"
+	fill="currentColor"
+	{...restProps}
+>
+	{@render children()}
+</svg>
