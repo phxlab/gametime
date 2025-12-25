4e9b84ba618127c31520d21f93c62e199c56738b
Nico Plyley <nico@phoenixtechlab.com>
2025-12-25 17:15:39 -0500

    feat(icons): export icons

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Add } from './Add.svelte';
+export { default as BackArrow } from './BackArrow.svelte';
+export { default as Company } from './Company.svelte';
+export { default as Search } from './Search.svelte';
