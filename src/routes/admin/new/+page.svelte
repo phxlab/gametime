995d5533e2a5e24ff7a8e62ee013fcb894d64768
Nico Plyley <nico@phoenixtechlab.com>
2025-12-25 17:18:01 -0500

    refactor(admin): replace `Page` component with custom layout in new organization page

--- src/routes/admin/new/+page.svelte
+++ src/routes/admin/new/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	import { OrgForm, Page } from '$lib/components/admin/forms';
+	import { OrgForm } from '$lib/components/admin/forms';
+	import { resolve } from '$app/paths';
+	import { BackArrow } from '$lib/components/icons';
 
 	const { data } = $props();
 </script>
 
-<Page
-	previousPage="/admin"
-	previousText="Back to Dashboard"
-	title="Create Organization"
-	subtitle="Add a new client organization to the system"
->
+<svelte:head>
+	<title>Create Organization</title>
+</svelte:head>
+
+<div class="@container mt-5">
+	<div class="mb-6">
+		<a
+			href={resolve('/admin')}
+			class="group text-gray-500 transition-colors hover:text-gray-600 hover:dark:text-gray-400"
+		>
+			<div
+				class="inline-block transition-transform duration-200 ease-out group-hover:-translate-x-1"
+			>
+				<BackArrow />
+			</div>
+			Back to Dashboard
+		</a>
+		<h1 class="text-2xl font-bold">Create Organization</h1>
+	</div>
+
 	<OrgForm formData={data.form} />
-</Page>
+</div>
