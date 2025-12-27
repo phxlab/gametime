f08bb01b17756a275730c6bb5e936186f8edbd6c
Nico Plyley <nico@phoenixtechlab.com>
2025-12-27 14:03:14 -0700

    refactor(admin): replace BackArrow with Back component in new organization page

--- src/routes/admin/new/+page.svelte
+++ src/routes/admin/new/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { OrgForm } from '$lib/components/admin/forms';
-	import { resolve } from '$app/paths';
-	import { BackArrow } from '$lib/components/icons';
+	import { Back } from '$lib/components/admin/ui';
 
 	const { data } = $props();
 </script>
@@ -12,18 +11,8 @@
 
 <div class="@container mt-5">
 	<div class="mb-6">
-		<a
-			href={resolve('/admin')}
-			class="group text-gray-500 transition-colors hover:text-gray-600 hover:dark:text-gray-400"
-		>
-			<div
-				class="inline-block transition-transform duration-200 ease-out group-hover:-translate-x-1"
-			>
-				<BackArrow />
-			</div>
-			Back to Dashboard
-		</a>
-		<h1 class="text-2xl font-bold">Create Organization</h1>
+		<Back title="Back to Dashboard" href="/admin" />
+		<h1 class="text-xl font-bold">Create Organization</h1>
 	</div>
 
 	<OrgForm formData={data.form} />
