e7f2f440141facee044003ae9bc78803d1eaa27a
Nico Plyley <nico@phoenixtechlab.com>
2025-12-28 14:10:10 -0700

    feat(icons): add new Calendar, Gear, Shirt, and Tag icons

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,4 +1,8 @@
 export { default as Add } from './Add.svelte';
 export { default as BackArrow } from './BackArrow.svelte';
+export { default as Calendar } from './Calendar.svelte';
 export { default as Company } from './Company.svelte';
+export { default as Gear } from './Gear.svelte';
 export { default as Search } from './Search.svelte';
+export { default as Shirt } from './Shirt.svelte';
+export { default as Tag } from './Tag.svelte';
